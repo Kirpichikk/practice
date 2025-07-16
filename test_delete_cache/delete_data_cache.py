0a93cf712d2cfb4c03cb819d65f9f26b7139b302
Kirpichikk <lady.kirpichik@yandex.ru>
2025-07-16 15:42:44 +0300

    update

---
+++ test_delete_cache/delete_data_cache.py
@@ -0,0 +1,16 @@
+from connection import Connection
+
+def delete_data(conn, space_name, key):
+    for i in key:
+        space = conn.call(f"box.space.{space_name}:get", (i,))
+        if space:
+            delete_result = conn.call(
+                f"box.space.{space_name}:delete",
+                (key,)
+            )
+            return delete_result
+        else:
+            return "нет возможности удаления по данному ключу"
+
+conn = Connection()
+print(delete_data(conn,"cache1", "2"))
--- test_update_cache/update_data_cache.py
+++ test_update_cache/update_data_cache.py
@@ -5,7 +5,7 @@ def update_value(conn, space_name, key, new_value):
         if space:
             update_result = conn.call(
                 f"box.space.{space_name}:update",
-                (key, [("=", "value", new_value)])  # Исправлено здесь
+                (key, [("=", "value", new_value)])
             )
             return update_result
         else:
